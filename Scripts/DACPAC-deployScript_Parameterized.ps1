fcbeae9c3d6cf26257034ee610c4ac23ebb5570a
RavitejaAmruthapu <ateja0@gmail.com>
2019-03-11 01:44:27 +0530

    Update DACPAC-deployScript_Parameterized.ps1

--- Scripts/DACPAC-deployScript_Parameterized.ps1
+++ Scripts/DACPAC-deployScript_Parameterized.ps1
@@ -1,9 +1,9 @@
 #-- to run this script we need to pass parameter
 #-tcDir %teamcity.build.checkoutDir%
 #-dacpac_location %dacpac_location%
-param([string]$tcDir = "%teamcity.build.checkoutDir%" )
-param([string]$dacpac_location = "%dacpac_location%" )
+param([string]$tcDir = "" )
+#param([string]$dacpac_location = "%dacpac_location%" )
 Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll"
 $dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=(local)"
-$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\$dacpac_location")
+$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir")
 $dacService.deploy($dacPac, "QA", "True")
